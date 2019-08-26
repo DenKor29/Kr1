0c4b3c567acc6caa6d1cc1cdaf6e77b5158c0235
DenKor29 <denis.p.korablev@gmail.com>
2019-08-26 15:32:37 +0300

    Initial commit

---
+++ database/src/kr1/database/DBServerListener.java
@@ -0,0 +1,10 @@
+package kr1.database;
+
+import avayacdr.core.AvayaCDRData;
+import avayacdr.http.HTTPRequest;
+
+import java.util.ArrayList;
+
+public interface DBServerListener {
+    void onRecivedCDR(DBServer dbServer, HTTPRequest httpRequest, ArrayList <AvayaCDRData> cdrData);
+}
